5cf89336b4be5815f51a31fbb3c99dd36de49e62
cbackstrom80 <curtis.backstrom@gmail.com>
2021-04-22 08:36:01 -0700

    Add all

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 from flask import Flask
+import uwsgidecorators
+from splunk_otel.tracing import start_tracing
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+
 app = Flask(__name__)
+
+@uwsgidecorators.postfork
+def setup_tracing():
+    start_tracing()
+    # instrument our flask app instance eplicitly
+    FlaskInstrumentor().instrument_app(app)
 
 @app.route("/")
 def hello():
